ec7eb56073936061cdc652078b85c047fb2cfe01
Raj Sharma <sharmaraj.sm@gmail.com>
2025-02-01 15:08:35 +0530

    Your commit message

--- App.py
+++ App.py
@@ -4,6 +4,26 @@ import pandas as pd
 import os
 import requests
 
+def fetch_movie_details(movie_id):
+    api_key = "ab51797faa0023324ad58192ad117ae9"
+    movie_url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
+    cast_url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={api_key}&language=en-US"
+    
+    movie_data = requests.get(movie_url).json()
+    cast_data = requests.get(cast_url).json()
+    
+    poster_url = "https://image.tmdb.org/t/p/w500/" + movie_data['poster_path']
+    summary = movie_data.get('overview', 'No summary available')
+    
+    cast_list = []
+    for member in cast_data.get('cast', [])[:5]:  # Show top 5 cast members
+        cast_list.append({
+            'name': member['name'],
+            'profile': "https://image.tmdb.org/t/p/w500/" + member['profile_path'] if member.get('profile_path') else None
+        })
+    
+    return poster_url, summary, cast_list
+
 def fetch_posters(movie_id):
    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=ab51797faa0023324ad58192ad117ae9&language=en-US '.format(movie_id))
    data = response.json()
@@ -51,6 +71,21 @@ if st.button('Recommend'):
                     
                     st.image(posters[i + j])
 
+if 'selected_movie' in st.session_state:
+    movie_details = fetch_movie_details(st.session_state['selected_movie']['id'])
+    st.subheader(st.session_state['selected_movie']['title'])
+    st.image(movie_details[0], width=300)
+    st.write(movie_details[1])
+    st.subheader("Cast:")
+    for cast in movie_details[2]:
+        col1, col2 = st.columns([1, 3])
+        with col1:
+            if cast['profile']:
+                st.image(cast['profile'], width=100)
+        with col2:
+            st.write(cast['name'])
+
+
 
 
 
@@ -73,11 +108,3 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
-   
-
-
-
-
-
-
